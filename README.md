b8eaf53db2f96ddcbc6066be98fec91a39cfaecf
Luz <luz@eurekapublicidad.com>
2021-08-03 11:40:51 -0600

    header with basic styling

---
+++ README.md
@@ -0,0 +1,3 @@
+Attributions
+
+- Photo by Alexy Almond on Pexels [link](https://www.pexels.com/es-es/foto/foto-de-patatas-en-bandeja-de-porcelana-3758133/)
---
+++ src/components/Layout/Header.js
@@ -0,0 +1,18 @@
+import styles from './Header.module.css';
+import headerImage from '../../assets/header.jpg';
+
+const Header = () => {
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>ReactiveFood</h1>
+        <button>Cart</button>
+      </header>
+      <div className={styles['main-image']}>
+        <img src={headerImage} alt='A table with different food' />
+      </div>
+    </>
+  );
+};
+
+export default Header;
